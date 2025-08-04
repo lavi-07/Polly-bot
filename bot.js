995f66d34c35feb06356930e017dad7d758b5ad2
lavi-07 <001dell05@gmail.com>
2025-08-04 13:37:42 +0530

    start

--- bot.js
+++ bot.js
@@ -36,36 +36,37 @@ bot.onText(/\/start/, (msg) => {
   `, {
     parse_mode: "Markdown"
   });
-});
-
-
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-  if(msg.video){
-    console.log(msg.video.file_id)
-  }else{
-    const found = data.filter(item => item.title.includes(msg.text)
-    );
-    if(found.length !== 0){
-    const keybord =found.map(item=>[{text:item.title,callback_data: item.title}])
-    bot.sendMessage(chatId, "Your Movies LIst", {
-      reply_markup: {
-        inline_keyboard: keybord
+  bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    if(msg.video){
+      console.log(msg.video.file_id)
+    }else{
+      const found = data.filter(item => item.title.includes(msg.text)
+      );
+      if(found.length !== 0){
+      const keybord =found.map(item=>[{text:item.title,callback_data: item.title}])
+      bot.sendMessage(chatId, "Your Movies LIst", {
+        reply_markup: {
+          inline_keyboard: keybord
+        }
+      });}else{
+        bot.sendMessage(chatId, `
+  ❌ *No Results Found*
+  ──────────────
+  🔍 Please check your spelling or try again.
+  
+  📝 Make sure you typed the title correctly, just like it appears on Google.
+  `);
+  
       }
-    });}else{
-      bot.sendMessage(chatId, `
-❌ *No Results Found*
-──────────────
-🔍 Please check your spelling or try again.
-
-📝 Make sure you typed the title correctly, just like it appears on Google.
-`);
-
     }
-  }
-  
-  
+    
+    
+  });
 });
+
+
+
 bot.on("callback_query",(query)=>{
   const chatId = query.message.chat.id;
   const chossenTi = query.data; // This is your "some_action"
